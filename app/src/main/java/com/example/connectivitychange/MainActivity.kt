a26bcd25f7657a94cbf6c149995eb9c2152a8dd3
ANN KARUKU <annkaruku5832@gmail.com>
2022-11-01 02:47:27 +0300

    Added get network state on connectivityReceiver.kt

--- app/src/main/java/com/example/connectivitychange/MainActivity.kt
+++ app/src/main/java/com/example/connectivitychange/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.connectivitychange
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var connReceiver: connectivityReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
     }
 }
--- app/src/main/java/com/example/connectivitychange/connectivityReceiver.kt
+++ app/src/main/java/com/example/connectivitychange/connectivityReceiver.kt
@@ -3,10 +3,13 @@ package com.example.connectivitychange
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class connectivityReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        
-    }
 
+        val status: String = Networkstatus.getConnectivityStatusString(context)
+
+        Toast.makeText(context, status, Toast.LENGTH_LONG).show()
+    }
 }
