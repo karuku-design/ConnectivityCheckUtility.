af9d181e9f3fefcff50528d21f48cc7838086294
ANN KARUKU <annkaruku5832@gmail.com>
2022-10-31 16:06:12 +0300

    Created new connectivityBroadcastReceiver class.

---
+++ app/src/main/java/com/example/connectivitychange/connectivityReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.connectivitychange
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class connectivityReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        
+    }
+
+}
